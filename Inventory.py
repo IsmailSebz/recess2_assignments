e8abfd23ee654bb75c93cfede965f29798a87486
IsmailSebz <tsebz07@gmail.com>
2025-05-23 01:42:27 +0300

    added table style display

--- Inventory.py
+++ Inventory.py
@@ -2,12 +2,16 @@
 #
 
 #Initialise a list data structure to store the available items
-stock =[]
+stock =[
+		{"name":"Muwogo","price":"20000"},
+		{"name":"rice","price":"300000"}
+	]
 choice=""
 item_name=""
 item_price=""
 
 def add_item():
+	item={"name":"","price":""}
 	print("Enter Item Name")
 	item_name=input(">>:")
 	print(f"Enter {item_name}'s Price")
@@ -16,44 +20,81 @@ def add_item():
 		add_item() 
 
 	#INCASE THE LENGTH IS MORE, CODE WILL CONTINUE HERE
-	stock.append(item_name)
+	item["name"]=item_name
+	item["price"]=item_price
+	stock.append(item)
 	print("1. Enter Another")
 	print("2. Main Menu")
 	choice=""
 	choice=input(">>:")
-	if choice==1:
+	if choice=="1":
 		add_item() 
-	elif choice == 2:	
+	elif choice == "2":	
 		menu()
-        
-        
+
+##THE IDEA IS TO CREATE A TABLE LIKE VIEW
+def  view_stock_list():
+	GAP=2
+	headers=["No","Name","Price"]
+	max_len_no=len(headers[0])
+	max_len_name=len(headers[1])
+	max_len_price=len(headers[2])
 
 
+	#to get the column size
+	for i in stock:
+		if len(i["name"]) > max_len_name:max_len_name=len(i["name"])
+	for i in stock:
+		if len(i["price"]) > max_len_price:max_len_price=len(i["price"])
+	
+	#PRINT HEADERS
+	print("".ljust(max_len_no+max_len_name+max_len_price+GAP*6-2,"-"))
+	print("|"+
+		headers[0].center(max_len_no+GAP) + "|" + 
+	   	headers[1].center(max_len_name+GAP) + "|" +
+		headers[2].center(max_len_price+GAP) + "|"
+	   )
+	print("".ljust(max_len_no+max_len_name+max_len_price+GAP*6-2,"-"))
+	j=1
+	
+	#PRINT ITEMS IN STOCK
+	for i in stock:
+		print("|"+
+		str(j).ljust(max_len_no+GAP) + "|" +
+		i["name"].ljust(max_len_name+GAP) + "|" +
+		i["price"].rjust(max_len_price+GAP) + "|"
+		)
+		j+=1
 
 
 def menu():
-    print("==========WELCOME TO HAJ WAHAB TRADERS =======")
-    print("Choose Action to perform")
-    print("1. Add item to stock")
-    print("2. View Stock List")
-    print("3. Sell Item")
-    print("h. Help")
+	print("==========WELCOME TO HAJ WAHAB TRADERS =======")
+	print("Choose Action to perform")
+	print("1. Add item to stock")
+	print("2. View Stock List")
+	print("3. Sell Item")
+	print("h. Help")
+
+	while True:
+		choice=input(">>")    
+		if choice=="1":
+			add_item()
+			break
+		elif choice=="2":
+			view_stock_list()
+			break
+		elif choice =="3":
+			sell_item()
+			break
+		elif choice =="h":
+			inv_help()
+			break
+		else: 
+			print("Invalid Choice")
+			
+
 
 menu()
-while True:
-    choice=input(">>")    
-    if choice=="1":
-        add_item()
-        break
-    elif choice=="2":
-        view_stock()
-        break
-    elif choice =="3":
-        sell_item()
-        break
-    else: 
-         print("Invalid Choice")
-          
         
     
 
